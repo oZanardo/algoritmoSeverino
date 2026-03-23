5b2928c416ffd522b35c751e2fb37871913d207e
oZanardo <joaovzanardo@gmail.com>
2026-03-23 14:15:50 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        //Exercicio 1
+        System.out.println("Leia tres numeros inteiros e imprima a media aritmetica entre esses numeros");
+
+
+    }
+}
